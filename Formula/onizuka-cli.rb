3ea291edb011d2d3a4c6f5336b22f3b420ac89f3
James GIRARD <james.girard@biolevate.com>
2025-08-06 15:32:51 +0200

    Mise à jour de la formule onizuka-cli

--- Formula/onizuka-cli.rb
+++ Formula/onizuka-cli.rb
@@ -5,29 +5,29 @@ class OnizukaCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Biolevate/homebrew-tap/releases/download/cli-v1.0.8/gto-darwin-arm64.tar.gz"
-      sha256 "31163b60a897f3b4eaa2d22a8c6f6ab6ef55ba579dbfbf6639334d36c795df82"
+      url "https://github.com/Biolevate/homebrew-tap/releases/download/cli-v1.0.4/ozk-darwin-arm64.tar.gz"
+      sha256 "a664a6b5780e25e8dda46a5df562ae348754cc06c30978648a0047770da5d632"
     else
-      url "https://github.com/Biolevate/homebrew-tap/releases/download/cli-v1.0.8/gto-darwin-amd64.tar.gz"
-      sha256 "a45536c918bcb1228c0a04acf75900598b186636198869ae0d0d699a0bc829b3"
+      url "https://github.com/Biolevate/homebrew-tap/releases/download/cli-v1.0.4/ozk-darwin-amd64.tar.gz"
+      sha256 "9784d523595adb96e797296a974e6aa36221e7ddabe35aa1a5033783ffb153d1"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Biolevate/homebrew-tap/releases/download/cli-v1.0.8/gto-linux-arm64.tar.gz"
-      sha256 "23d44194d384b5dd468dfdc63b1fa37f672d01d9046bcf9e9824134875cc4b60"
+      url "https://github.com/Biolevate/homebrew-tap/releases/download/cli-v1.0.4/ozk-linux-arm64.tar.gz"
+      sha256 "ab280b65a48f2312c6ec4913d3128648280834a9c82526acdb02aad350f51269"
     else
-      url "https://github.com/Biolevate/homebrew-tap/releases/download/cli-v1.0.8/gto-linux-amd64.tar.gz"
-      sha256 "52a2fb0e19986916e1807582585b17afa1dc680dac7dd61353b8d975f9f8787b"
+      url "https://github.com/Biolevate/homebrew-tap/releases/download/cli-v1.0.4/ozk-linux-amd64.tar.gz"
+      sha256 "a50bec981977a05d7dfd0c424f8bb89bdfeafd8dded6caf2fc9fd799173cd319"
     end
   end
 
   def install
-    bin.install "gto"
+    bin.install "ozk"
   end
 
   test do
-    system "#{bin}/gto", "--version"
+    system "#{bin}/ozk", "--version"
   end
 end 
